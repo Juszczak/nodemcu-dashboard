b7f0e17b472f598335e875a23a41f24c66ad35ed
Adrian Juszczak <adrian@juszczak.io>
2016-01-23 17:23:23 +0100

    routable home and dashboard components

---
+++ src/app/dashboard.component.ts
@@ -0,0 +1,12 @@
+import {
+	Component
+} from 'angular2/core';
+
+@Component({
+	directives: [
+	],
+	selector: 'dashboard',
+	template: `hello from dashboard component`
+})
+export class DashboardComponent {
+}
---
+++ src/app/home.component.ts
@@ -0,0 +1,12 @@
+import {
+	Component
+} from 'angular2/core';
+
+@Component({
+	directives: [
+	],
+	selector: 'home',
+	template: `hello from home component`
+})
+export class HomeComponent {
+}
